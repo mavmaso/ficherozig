77ad153609cdf28564c391d189cf75d902f39d94
Marcos Soares <mavmaso@gmail.com>
2026-05-30 18:02:55 -0300

    add io with threaded

--- main.zig
+++ main.zig
@@ -2,10 +2,8 @@ const std = @import("std");
 const csv_core = @import("csv_core.zig");
 
 pub fn main() !void {
-    var gpa = std.heap.DebugAllocator(.{}){};
-    defer _ = gpa.deinit();
-
-    var threaded = std.Io.Threaded.init(gpa.allocator(), .{});
+    const allocator = std.heap.page_allocator;
+    var threaded = std.Io.Threaded.init(allocator, .{});
     const io = threaded.io();
 
     const metadata = csv_core.Metadata{ .new_path = "temp/output.csv", .country_code = "", .has_accent = false };
